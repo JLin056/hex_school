22bcdd4f4e8f7bef36f74a663fbfc5759108b8ae
JLin <jlin890516@gmail.com>
2025-04-28 23:59:53 +0800

    daily practice - day1

---
+++ dailyPractice/JS/Day1/all.js
@@ -0,0 +1,24 @@
+// 第一題
+function input(){
+    return "Hello world!";
+}
+
+// 第二題
+function sayHi(name){
+    return `${name} , Hello world!`;
+}
+
+// 第三題
+function add(num){
+    return num *2;
+}
+
+// 第四題
+function addTwo(num1, num2){
+    return num1 + num2;
+}
+
+// 第五題
+function addNumberAndString(num1, num2, num3){
+    return `第一加第二個參數加總為 ${num1+num2}, 第三個參數為 ${num3}`; 
+}

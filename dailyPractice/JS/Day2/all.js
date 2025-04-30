a888270fa22393cb494490f723bfa55357460322
JLin <jlin890516@gmail.com>
2025-04-30 13:46:13 +0800

    Daily practice Day2

---
+++ dailyPractice/JS/Day2/all.js
@@ -0,0 +1,24 @@
+// 第一題
+function square(num) {
+    return num ** 2;
+}
+
+// 第二題
+function calculate(num1, num2, num3){
+    return num1+num2-num3;
+}
+
+// 第三題
+function rounding(num){
+    return num.toFixed(2);
+}
+
+// 第四題
+function parseNumber(value){
+    return Number(value);
+}
+
+// 第五題
+function divisible(num){
+    return num % 2;
+}

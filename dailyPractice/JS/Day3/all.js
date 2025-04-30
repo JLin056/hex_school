e70f2316967ceb2d5047e4ed9f20f9eb7c1850df
JLin <jlin890516@gmail.com>
2025-04-30 15:33:56 +0800

    daily practice - day3

---
+++ dailyPractice/JS/Day3/all.js
@@ -0,0 +1,24 @@
+// 第一題：字串長度計算
+function checkStringLength(text){
+    return text.length;
+}
+
+// 第二題：英文強制小寫
+function lowerCase(text){
+    return text.toLowerCase(text);
+}
+
+// 第三題：去除掉段落的左右空白
+function trimString(text){
+    return text.trim();
+}
+
+// 第四題：字串依照條件轉陣列
+function splitString(text){
+    return text.split(',');
+}
+
+// 第五題：切割市區字串
+function getArea(address){
+    return address.substring(3,6);
+}
